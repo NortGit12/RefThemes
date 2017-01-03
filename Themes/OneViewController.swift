99caf7b50f857b84a600556933446dabfabdc820
Jeff Norton <jeff12ntn@gmail.com>
2017-01-03 15:59:27 -0700

    Base views and images done

---
+++ Themes/OneViewController.swift
@@ -0,0 +1,40 @@
+//
+//  OneViewController.swift
+//  Themes
+//
+//  Created by Jeff Norton on 1/3/17.
+//  Copyright © 2017 JeffCryst. All rights reserved.
+//
+
+import UIKit
+
+class OneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Set the Navigation Bar's title image
+        let titleImageView = UIImageView(image: UIImage(named: "test-text-image"))
+        titleImageView.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        titleImageView.contentMode = UIViewContentMode.scaleAspectFit
+        
+        navigationItem.titleView = titleImageView
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
