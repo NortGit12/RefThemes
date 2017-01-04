77040a89bc4aa15030b18f77c26785dce8810a8c
Jeff Norton <jeff12ntn@gmail.com>
2017-01-04 15:50:46 -0700

    Theme now manages tint color

--- Themes/Theme.swift
+++ Themes/Theme.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017 JeffCryst. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 let SelectedThemeKey = "SelectedTheme"
 
@@ -21,6 +21,20 @@ enum Theme: Int {
     //==================================================
     // MARK: - Properties
     //==================================================
+    
+    /* Consumers
+        *UIApplication delegate window's tint color
+    */
+    var mainColor: UIColor {
+        switch self {
+        case .dark:
+            return UIColor(red: 242/255.0, green: 101/255.0, blue: 34/255.0, alpha: 1.0)  // orange
+        case .graphical:
+            return UIColor(red: 10/255.0, green: 10/255.0, blue: 10/255.0, alpha: 1.0)    // black
+        case .normal:
+            return UIColor(red: 87/255.0, green: 188/255.0, blue: 95/255.0, alpha: 1.0)   // light green
+        }
+    }
 }
 
 struct ThemeManager {
@@ -33,6 +47,9 @@ struct ThemeManager {
         // Save the Theme value in UserDefaults and write the changes
         UserDefaults.standard.setValue(theme.rawValue, forKey: SelectedThemeKey)
         UserDefaults.standard.synchronize()
+        
+        // Change the tint color
+        UIApplication.shared.delegate?.window??.tintColor = theme.mainColor
     }
     
     static func currentTheme() -> Theme? {
