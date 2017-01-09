ecc3380d5f8a92a84db4c00588620c194ef4583e
Jeff Norton <jeff12ntn@gmail.com>
2017-01-09 13:37:41 -0700

    Theme now supports custom segmented control

--- Themes/Theme.swift
+++ Themes/Theme.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017 JeffCryst. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 let SelectedThemeKey = "SelectedTheme"
 
@@ -17,6 +17,24 @@ enum Theme: Int {
     //==================================================
     
     case dark, graphical, normal
+    
+    //==================================================
+    // MARK: - Computed Properties
+    //==================================================
+    
+    /* Consumers
+     *UIApplication delegate window's tint color
+     */
+    var mainColor: UIColor {
+        switch self {
+        case .dark:
+            return UIColor(red: 242/255.0, green: 101/255.0, blue: 34/255.0, alpha: 1.0)  // orange
+        case .graphical:
+            return UIColor(red: 10/255.0, green: 10/255.0, blue: 10/255.0, alpha: 1.0)    // black
+        case .normal:
+            return UIColor(red: 87/255.0, green: 188/255.0, blue: 95/255.0, alpha: 1.0)   // light green
+        }
+    }
 }
 
 struct ThemeManager {
@@ -29,6 +47,20 @@ struct ThemeManager {
         // Save the Theme value in UserDefaults and write the changes
         UserDefaults.standard.setValue(theme.rawValue, forKey: SelectedThemeKey)
         UserDefaults.standard.synchronize()
+        
+        // Change the tint color
+        UIApplication.shared.delegate?.window??.tintColor = theme.mainColor
+        
+        // Customize the segmented control
+        let controlBackgroundRegular = UIImage(named: "controlBackgroundRegular")?
+            .withRenderingMode(.alwaysTemplate)
+            .resizableImage(withCapInsets: UIEdgeInsets(top: 3.0, left: 3.0, bottom: 3.0, right: 3.0))
+        let controlBackgroundSelected = UIImage(named: "controlBackgroundSelected")?
+            .withRenderingMode(.alwaysTemplate)
+            .resizableImage(withCapInsets: UIEdgeInsets(top: 3.0, left: 3.0, bottom: 3.0, right: 3.0))
+        
+        UISegmentedControl.appearance().setBackgroundImage(controlBackgroundRegular, for: .normal, barMetrics: .default)
+        UISegmentedControl.appearance().setBackgroundImage(controlBackgroundSelected, for: .selected, barMetrics: .default)
     }
     
     static func currentTheme() -> Theme? {
