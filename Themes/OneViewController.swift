8632638812bd9c938c260d0389ee2fe30372cb42
Jeff Norton <jeff12ntn@gmail.com>
2017-01-09 16:33:43 -0700

    Theme supports single custom segmented control

--- Themes/OneViewController.swift
+++ Themes/OneViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class OneViewController: UIViewController {
+    
+    //==================================================
+    // MARK: - _Properties
+    //==================================================
+    
+    @IBOutlet weak var animalTypeSegmentedControl: UISegmentedControl!
+    
+    //==================================================
+    // MARK: - View Lifecycle
+    //==================================================
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,22 +29,8 @@ class OneViewController: UIViewController {
         titleImageView.contentMode = UIViewContentMode.scaleAspectFit
         
         navigationItem.titleView = titleImageView
+        
+        animalTypeSegmentedControl.setImage(UIImage(named: "cat")?.withRenderingMode(.alwaysTemplate), forSegmentAt: 0)
+        animalTypeSegmentedControl.setImage(UIImage(named: "dog")?.withRenderingMode(.alwaysTemplate), forSegmentAt: 1)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
