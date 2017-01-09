6f695e03f26d30bc54da5b931a83951e28715c13
Jeff Norton <jeff12ntn@gmail.com>
2017-01-09 11:51:37 -0700

    Theme now supports tab bar selection indicator
    …*The tab bar selection indicator is now generated
    …*The tint color is supported by the theme

--- Themes/OneViewController.swift
+++ Themes/OneViewController.swift
@@ -19,22 +19,15 @@ class OneViewController: UIViewController {
         titleImageView.contentMode = UIViewContentMode.scaleAspectFit
         
         navigationItem.titleView = titleImageView
+
+        ThemeManager.tabBarController = self.tabBarController
+        
+        // Retrieve the stored theme and apply it
+        guard let currentTheme = ThemeManager.currentTheme() else {
+            NSLog("Error identifying the stored theme.")
+            return
+        }
+        
+        ThemeManager.applyTheme(currentTheme)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Themes/Theme.swift
+++ Themes/Theme.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017 JeffCryst. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 let SelectedThemeKey = "SelectedTheme"
 
@@ -17,9 +17,30 @@ enum Theme: Int {
     //==================================================
     
     case dark, graphical, normal
+    
+    //==================================================
+    // MARK: - Computed Properties
+    //==================================================
+    
+    /*
+     Consumers:
+     *UIApplication delegate window's tint color
+     */
+    var mainColor: UIColor {
+        switch self {
+        case .normal:
+            return UIColor(red: 87/255.0, green: 188/255.0, blue: 95/255.0, alpha: 1.0)   // light green 
+        case .dark:
+            return UIColor(red: 242/255.0, green: 101/255.0, blue: 34/255.0, alpha: 1.0)  // orange
+        case .graphical:
+            return UIColor(red: 10/255.0, green: 10/255.0, blue: 10/255.0, alpha: 1.0)    // black 
+        }
+    }
 }
 
 struct ThemeManager {
+    
+    static var tabBarController: UITabBarController?
     
     //==================================================
     // MARK: - Methods
@@ -29,6 +50,15 @@ struct ThemeManager {
         // Save the Theme value in UserDefaults and write the changes
         UserDefaults.standard.setValue(theme.rawValue, forKey: SelectedThemeKey)
         UserDefaults.standard.synchronize()
+        
+        // Customize the tab bar's selection indicator
+        if let tabBarController = tabBarController {
+            let tabBarSelectionIndicatorLine = UIImage().createTabBarSelectionIndicatorLine(color: theme.mainColor
+                , size: CGSize(width: (tabBarController.tabBar.frame.width) / CGFloat(tabBarController.tabBar.items!.count)
+                    , height: (tabBarController.tabBar.frame.height))
+                , lineWidth: 3.0)
+            tabBarController.tabBar.selectionIndicatorImage = tabBarSelectionIndicatorLine
+        }
     }
     
     static func currentTheme() -> Theme? {
@@ -46,3 +76,39 @@ struct ThemeManager {
         }
     }
 }
+
+extension UIImage {
+    func createTabBarSelectionIndicatorLine(color: UIColor, size: CGSize, lineWidth: CGFloat) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        color.setFill()
+        UIRectFill(CGRect(x: 0, y: size.height - lineWidth, width: size.width, height: lineWidth))
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
