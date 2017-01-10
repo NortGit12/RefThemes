e966c9d93634411fda7a7cf5087b5c703d9e8a42
Jeff Norton <jeff12ntn@gmail.com>
2017-01-10 12:12:56 -0700

    Code look cleanup

--- Themes/OneViewController.swift
+++ Themes/OneViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class OneViewController: UIViewController {
+    
+    //==================================================
+    // MARK: - View Lifecycle
+    //==================================================
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,21 +24,4 @@ class OneViewController: UIViewController {
         
         navigationItem.titleView = titleImageView
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Themes/SettingsViewController.swift
+++ Themes/SettingsViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    //==================================================
+    // MARK: - View Lifecycle
+    //==================================================
 
     override func viewDidLoad() {
         super.viewDidLoad()
